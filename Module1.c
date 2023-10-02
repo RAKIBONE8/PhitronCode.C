448d57616810376f90d2bdf83cc32397ad51eca9
RAKIBONE8 <rmvk187@gamil.com>
2023-10-02 14:36:58 +0530

    2nd push

---
+++ Module1.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+/*
+int main ()
+{
+    printf("Rakib is a peaceful soul\n");
+    printf("\tNoob coder");
+
+    return 0;
+}
+*/
+
+/*
+ESCAPE character
+ \n - new line
+ \t - tab
+*/
+
+/*
+int main ()
+{
+    // int rakib = 18;
+    // int x = 10;
+    // int y = 12;
+
+    // printf("%d %d %d",rakib,x,y);
+
+    // float effort = 120.99;
+
+    // printf("%0.2f",effort);
+
+    char Goku = 'S';
+    printf("%c",Goku);
+
+    return 0;
+}
+*/
+
+/*
+int main ()
+{
+    // int king,queen;
+    // char c;
+    // float f;
+
+    // scanf("%d %c %f",&king,&c,&f);
+    // printf("%d %c %0.2f",king,c,f);
+
+    int a,b;
+
+    scanf("%da %db",&a,&b);
+    printf("%da %db",a,b);
+
+    return 0;
+}
+*/
+
+int main ()
+{
+    // int  a = 1000000000; //(-10^9 - +10^9);
+    // long long int b = 1000000000000000000; //(-10^18 - +10^18);
+    // printf("%lld",b);
+
+    double a = 2006.8439652920843;
+
+    printf("%0.15lf",a);
+    return 0;
+}
+
+//Rakib
