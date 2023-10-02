7f120b0440d127e2e96f733a8f7fa0e67c6b43e5
Rakib Mondal <143431829+RAKIBONE8@users.noreply.github.com>
2023-10-02 23:12:57 +0530

    normally

---
+++ Module2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+//Arithmetic operator
+/*
+int main ()
+{
+    int a=10,b=20;
+    int sum=a+b;
+    printf("%d \n",sum);
+
+    int mns = b-a;
+    printf("%d\n",mns);
+    
+    int x = 3;
+    float div = b*1.0/x;
+    printf("%f\n",div);
+
+    int mul = a*b;
+    printf("%d\n",mul);
+
+    int mod = a%b;
+    printf("%d",mod);
+}
+*/
