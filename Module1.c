d81b114ea625e4d2b6b96b5e46b2c2a8d3b6b05e
Rakib Mondal <143431829+RAKIBONE8@users.noreply.github.com>
2023-10-02 14:59:42 +0530

    Adding pattern

--- Module1.c
+++ Module1.c
@@ -54,6 +54,7 @@ int main ()
 }
 */
 
+/*
 int main ()
 {
     // int  a = 1000000000; //(-10^9 - +10^9);
@@ -65,5 +66,25 @@ int main ()
     printf("%0.15lf",a);
     return 0;
 }
+*/
 
 //Rakib
+
+int main ()
+{
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=n;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
